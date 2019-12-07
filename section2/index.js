cc0e47c440f8785cf0f2b87e2ca3efc44f640597
karaevelize <k.evelize@gmail.com>
2019-12-07 11:28:33 +0100

    Fixed the error, router working

--- section2/index.js
+++ section2/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const port = 4000;
-const app = express();
-const router = require("./movies/router");
+const db = require("./sequelize-rest");
 const bodyParser = require("body-parser");
+const router = require("./movies/router");
+
+const app = express();
 
 const parserMiddleware = bodyParser.json();
 app.use(parserMiddleware);
 
-app.listen(router);
+app.use(router);
+
 app.listen(port, () => console.log(`Hey, I'm listening on port ${port}!`));
