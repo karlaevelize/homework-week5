d896593cba5ea055563923de7a06a9a8e4c8faa8
karaevelize <k.evelize@gmail.com>
2019-12-07 12:34:36 +0100

    text loggin to the console and message working

--- section1/messages-api.js
+++ section1/messages-api.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.post("/messages", (request, respose) => {});
+app.use(bodyParser.json());
+app.post("/messages", (request, response) => {
+  const text = request.body.text;
+  console.log(request.body.text);
+  response.json({
+    message: "Message received loud and clear"
+  });
+});
 
-app.listen(port);
+app.listen(port, () => console.log(`I'm listenin on port ${port}`));
